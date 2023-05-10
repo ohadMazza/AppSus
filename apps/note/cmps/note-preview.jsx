6c966bd022532eeab2a684b6784363922bb34e9e
Efrat Leibenzon <efrat.leibenzon@gmail.com>
2023-05-10 22:59:48 +0300

    test

--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -2,13 +2,8 @@ export function NotePreview({ note }) {
 
     return (
         <article contentEditable className="note-preview">
-<<<<<<< HEAD
             <h3>text{note.info.txt}</h3>
             <h6>created at {note.createdAt}</h6>
-=======
-            <h3>text:{note.info.txt}</h3>
-            <h6>created at:{note.createdAt}</h6>
->>>>>>> cc05bcdd011273ea718f04b77df3364b534271fa
         </article>
     )
 }
