f7fef4bcf642cfcf9770b1a047bfb4c3f1fd2f6e
ohadMazza <ohadmazza@gmail.com>
2023-05-13 22:51:27 +0300

    compose mail

--- apps/mail/cmps/mail-sort.jsx
+++ apps/mail/cmps/mail-sort.jsx
@@ -3,19 +3,16 @@ const { useState, useEffect } = React
 
 export function MailSort({ setSortOption, setSortOrder, sortOrder }) {
 
-
-
-
     return (
         <div className="sort-section">
             <span>Sort by:</span>
             <div className="sort-section-btns">
-                <button className="sort-btn" >
+                {/* <button className="sort-btn" >
                     Read
                 </button >
                 <button className="sort-btn">
                     Unread
-                </button>
+                </button> */}
                 <button className="sort-btn" onClick={() => {
                     setSortOption("date")
                     setSortOrder(sortOrder * -1)
